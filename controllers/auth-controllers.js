b4f40d8fb6be1ab61ef94c47c05440759429e9e0
Ewanescence <ewanescence91@gmail.com>
2021-08-11 09:26:09 +0200

    # Security 3 #
    This commit includes :
    • Some node-modules update
    • Hashing emails

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -8,45 +8,53 @@ const User = require('../models/auth-model') // Schéma de données utilisateur
 // Contrôleur d'authentification
 
 exports.signUp = (req, res, next) => { // Logique d'inscription
-    bcrypt.hash(req.body.email, 10)
-        .then(hemail => {
+    bcrypt.hash(req.body.email, 10) // On sale l'e-mail récupérée dans le corps de la requête 10 fois
+        .then(hash => {
+            const user = new User({ // Déclaration d'un nouvel objet utilisateur
+                email: hash, // Email récupéré dans la requête puis hashé
+                password: 'test' // Mot de passe basique, il sera écrasé ensuite
+            });
             bcrypt.hash(req.body.password, 10) // On sale le mot de passe récupéré dans le corps de la requête 10 fois
-                .then(hash => {
-                    const user = new User({ // Déclaration d'un nouvel objet utilisateur
-                        email: hemail, // Email récupéré dans la requête
-                        password: hash // Mot de passe récupéré puis hashé 
-                    });
-                    user.save() // Sauvegarde de l'objet
-                        .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' })) // Réponse positive à la sauvegarde
-                        .catch(error => res.status(400).json({ error })); // Réponse négative à la sauvegarde
-                })
-                .catch(error => res.status(500).json({ error })); // Réponse négative au hashage du mot de passe 
+            .then(hash => {
+                user.password = hash // On écrase le paramètre password avec notre hash du mot de passe
+                user.save() // Sauvegarde de l'objet user
+                    .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' })) // Réponse positive à la sauvegarde
+                    .catch(error => res.status(400).json({ error })); // Réponse négative à la sauvegarde
+            })
+            .catch(error => res.status(500).json({ error })); // Réponse négative au hashage du mot de passe
         })
-        .catch(error => res.status(500).json({ error }));
-
+        .catch(error => res.status(500).json({ error })); // Réponse négative au hashage de l'adresse email
 };
 
-exports.login = (req, res, next) => { // Logique de connexion
-    User.findOne({ email: req.body.email }) // Cherche un utilisateur avec l'email de la requête 
+exports.login = async (req, res, next) => { // Logique de connexion
+    User.find() // Cherche un utilisateur avec l'email de la requête 
         .then(user => {
-            if (!user) { 
-                return res.status(401).json({ error: 'Utilisateur introuvable !' }); // Réponse négative si pas d'utilisateur trouvé
-            }
-            bcrypt.compare(req.body.password, user.password) // Hashage du mot de passe de la requête pour comparaison avec celui de l'utilisateur
-                .then(valid => { 
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' }); // Réponse négative si mauvais mot de passe
-                    }
-                    res.status(200).json({ // Réponse positive si bon mot de passe
-                        userId: user._id,
-                        token: jwt.sign(  // Création d'un token d'authentification temporaire à l'aide de l'id utilisateur et d'une chaîne
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error })); // Réponse négative en cas d'erreur du hashage
+            user.forEach(async (user) => {
+                const valid = await bcrypt.compare(req.body.email, user.email)
+                if (valid) { 
+                    await User.findOne({ email: user.email })
+                        .then(async (user) => {
+                            await bcrypt.compare(req.body.password, user.password)
+                            .then(valid => {
+                                if(valid) {
+                                    res.status(200).json({
+                                        userId: user._id,
+                                        token: jwt.sign(
+                                            { userId: user._id },
+                                            'RANDOM_TOKEN_SECRET',
+                                            { expiresIn: '24h' }
+                                        )
+                                    });
+                                }
+                                else {
+                                    return res.status(401).json({ error: 'Mot de passe incorrect' }); // Réponse négative si mauvais mot de
+                                }
+                            })
+                            .catch(error => res.status(401).json({ error })); // Réponse négative si erreur lors de la comparaison des mots de passe
+                        })
+                        .catch(error => res.status(401).json({ error })); // Réponse négative si erreur lors de la récupération de l'utilisateur
+                }
+            })
         })
         .catch(error => res.status(500).json({ error })); // Réponse négative à la récupération d'un utilisateur
 };
